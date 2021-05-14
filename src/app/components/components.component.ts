1f2f4bdfb9e7853ab6b57057ff41a988a5827d71
g14n4p3 <g14n4p3@g14n4p3s-MBP.fritz.box>
2021-05-14 09:00:01 +0200

    remove unused codes

--- src/app/components/components.component.ts
+++ src/app/components/components.component.ts
@@ -13,17 +13,6 @@ import * as Rellax from 'rellax';
 })
 
 export class ComponentsComponent implements OnInit, OnDestroy {
-    // data : Date = new Date();
-
-    // page = 4;
-    // page1 = 5;
-    // page2 = 3;
-    // focus;
-    // focus1;
-    // focus2;
-
-    // date: {year: number, month: number};
-    // model: NgbDateStruct;
 
     public isCollapsed = true;
     public isCollapsed1 = true;
@@ -35,14 +24,6 @@ export class ComponentsComponent implements OnInit, OnDestroy {
         config.closeOthers = true;
         config.type = 'info';
     }
-    // isWeekend(date: NgbDateStruct) {
-    //     const d = new Date(date.year, date.month - 1, date.day);
-    //     return d.getDay() === 0 || d.getDay() === 6;
-    // }
-
-    // isDisabled(date: NgbDateStruct, current: {month: number}) {
-    //     return date.month !== current.month;
-    // }
 
     ngOnInit() {
         const rellaxHeader = new Rellax('.rellax-header');
@@ -52,11 +33,6 @@ export class ComponentsComponent implements OnInit, OnDestroy {
         const body = document.getElementsByTagName('body')[0];
         body.classList.add('index-page');
     }
-
-    // scrollToElement($element): void {
-    //     console.log($element);
-    //     $element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
-    // }
 
     ngOnDestroy(){
         const navbar = document.getElementsByTagName('nav')[0];
